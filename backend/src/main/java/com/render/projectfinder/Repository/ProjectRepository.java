a1a94ae1133fdda93af2fe7c20136bb4f8e128fb
Blake Besecker <blake.besecker@gmail.com>
2025-10-23 23:27:15 -0400

    Update ProjectRepository.java

--- backend/src/main/java/com/render/projectfinder/Repository/ProjectRepository.java
+++ backend/src/main/java/com/render/projectfinder/Repository/ProjectRepository.java
@@ -2,6 +2,7 @@ package com.render.projectfinder.Repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,33 +12,18 @@ import com.render.projectfinder.Entity.Tag;
 
 public interface ProjectRepository extends JpaRepository<Project, Long> {
 
-    // Main query for project with given tags
-    @Query(
-        value = """
-        WITH tid AS (
-          SELECT id FROM tags WHERE lower(name) = ANY(cast(:taglist as text[]))
-        ),
-        pid AS (
-          SELECT project_id
-          FROM project_tags
-          WHERE tag_id IN (SELECT id FROM tid)
-          GROUP BY project_id
-          HAVING COUNT(DISTINCT tag_id) = (SELECT COUNT(*) FROM tid)
-        )
-        SELECT * 
-        FROM projects 
-        WHERE id IN (SELECT project_id FROM pid)
-        """,
-        nativeQuery = true
-    )
-    List<Project> findProjectsByAllTags(@Param("taglist") String[] taglist);
+    @Query("""
+        SELECT p
+        FROM Project p
+        JOIN p.tags t
+        WHERE LOWER(t.name) IN :taglist
+        GROUP BY p
+        HAVING COUNT(DISTINCT t.id) = :tagCount
+        """)
+    @EntityGraph(attributePaths = {"tags"})
+    List<Project> findProjectsByAllTags(@Param("taglist") List<String> taglist, @Param("tagCount") long tagCount);
 
-    // TODO: add a 'query for all tags in the database' function
-    @Query(
-        value = """
-        select * from tags
-        """,
-        nativeQuery = true
-    )
+    @Query("SELECT t FROM Tag t")
     List<Tag> getAllTags();
 }
+
